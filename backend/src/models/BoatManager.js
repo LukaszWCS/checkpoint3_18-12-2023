27f450827140f73f97fccf3277a571019dbf4922
lukasz <zed11temp@gmail.com>
2023-12-18 17:16:40 +0100

    step bonus

--- backend/src/models/BoatManager.js
+++ backend/src/models/BoatManager.js
@@ -5,7 +5,14 @@ class BoatManager extends AbstractManager {
     super({ table: "boat" });
   }
 
-  async readAll() {
+  async readAll(where) {
+    let whereString;
+    if (where !== undefined) {
+      whereString = Object.entries(where)
+        .map(([key, value]) => `b.${key}="${value}"`)
+        .join(" AND ");
+    }
+
     // Execute the SQL SELECT query to retrieve all boats from the "boat" table
     const [rows] = await this.database.query(
       `select 
@@ -19,7 +26,9 @@ class BoatManager extends AbstractManager {
         t.coord_y, 
         t.has_treasure 
       from ${this.table} b 
-      JOIN tile t ON b.coord_x=t.coord_x and b.coord_y=t.coord_y`
+      JOIN tile t ON b.coord_x=t.coord_x and b.coord_y=t.coord_y
+      ${whereString?.length > 0 ? `WHERE ${whereString}` : ""};
+      `
     );
 
     // Return the array of boats
